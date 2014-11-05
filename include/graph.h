a537bc52b1fe77d558679e32b2f40164763fcc29
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2014-11-05 01:28:00 -0200

    Order the arcs according to end_node

--- include/graph.h
+++ include/graph.h
@@ -11,23 +11,27 @@ class Graph {
   private:
     class ArcPointer {
       public:
-        ArcPointer (ArcType _info = 0, ArcPointer *n = 0, size_t en = 0,
-            size_t sn = 0) : info(_info), next(n), endNode(en),
-            startNode(sn) { };
-        ~ArcPointer () { };
+        ArcPointer (ArcType _info = 0, ArcPointer *n = 0, size_t sn = 0,
+            size_t en = 0) : info(_info), next(n), start_node(sn),
+            end_node(en) { };
+        ~ArcPointer () { destroy(); };
         ArcType info;
         ArcPointer *next;
-        size_t endNode;
-        size_t startNode;
+        size_t start_node;
+        size_t end_node;
 
         //Methods
         ArcPointer *getLast () {
           if (next == 0) return this; else return next->getLast(); };
+        void destroy () {
+          if (next != 0)
+            next->destroy();
+        }
     };
   public:
     Graph<NodeType, ArcType> () : status(gr_not_init), num_nodes(0),
         num_arcs(0), nodes(0), arcs(0) { };
-    ~Graph<NodeType, ArcType> () {};
+    ~Graph<NodeType, ArcType> () { destroy(); };
 
     void readGraph (const char *);
     void prettyPrint () const;
@@ -36,6 +40,39 @@ class Graph {
     size_t num_nodes, num_arcs;
     NodeType *nodes;
     ArcPointer **arcs;
+  private:
+    void init (size_t nn) {
+      if (nodes != 0 || arcs != 0)
+        throw("ERROR: Graph already initialized");
+      nodes = new NodeType[nn];
+      arcs = new ArcPointer*[nn];
+      for (size_t i = 0; i < nn; i++) {
+        ArcType info = ArcType();
+        arcs[i] = new ArcPointer(info, 0, i, nn);
+      }
+    }
+    void destroy () {
+      if (nodes != 0)
+        delete []nodes;
+      for (size_t i = 0; i < num_nodes; i++) {
+        arcs[i]->destroy();
+      }
+      if (arcs != 0)
+        delete []arcs;
+    }
+    void addArc (ArcType info, size_t from, size_t to) {
+      ArcPointer *arc = new ArcPointer(info, 0, from, to);
+      ArcPointer *p = arcs[from], *q = arcs[from];
+      while (p->end_node < to) {
+        q = p;
+        p = p->next;
+      }
+      if (p == arcs[from])
+        arcs[from] = arc;
+      else
+        q->next = arc;
+      arc->next = p;
+    }
 };
 
 /* file example:
@@ -49,20 +86,14 @@ template <typename NodeType, typename ArcType>
 void Graph<NodeType, ArcType>::readGraph (const char *filename) {
   std::ifstream file(filename);
   file >> num_nodes >> num_arcs;
-  nodes = new NodeType[num_nodes];
-  arcs = new ArcPointer*[num_nodes];
-  for (size_t i = 0; i < num_nodes; i++) {
+  init(num_nodes);
+  for (size_t i = 0; i < num_nodes; i++)
     file >> nodes[i];
-    arcs[i] = 0;
-  }
   size_t from, to;
   ArcType info;
   for (size_t k = 0; k < num_arcs; k++) {
     file >> from >> to >> info;
-    if (arcs[from] == 0)
-      arcs[from] = new ArcPointer(info, 0, to, from);
-    else
-      arcs[from]->getLast()->next = new ArcPointer(info, 0, to, from);
+    this->addArc(info, from, to);
   }
 }
 
@@ -73,8 +104,8 @@ void Graph<NodeType, ArcType>::prettyPrint () const {
   for (size_t i = 0; i < num_nodes; i++) {
     std::cout << "Node " << i << ": " << nodes[i] << std::endl;
     ArcPointer* p = arcs[i];
-    while (p != 0) {
-      std::cout << i << "->" << p->endNode << ": " << p->info << std::endl;
+    while (p->next != 0) {
+      std::cout << i << "->" << p->end_node << ": " << p->info << std::endl;
       p = p->next;
     }
   }
