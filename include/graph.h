8339f2a490d3035b6503f2e8ea8e4b5ba9db78aa
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2014-11-05 10:46:44 -0200

    Create member function create

--- include/graph.h
+++ include/graph.h
@@ -33,6 +33,23 @@ class Graph {
         num_arcs(0), nodes(0), arcs(0) { };
     ~Graph<NodeType, ArcType> () { destroy(); };
 
+    void create (size_t nn, NodeType *_nodes) {
+      init(nn);
+      for (size_t i = 0; i < nn; i++)
+        nodes[i] = _nodes[i];
+      status = gr_ok;
+    }
+    void destroy () {
+      if (nodes != 0)
+        delete []nodes;
+      for (size_t i = 0; i < num_nodes; i++) {
+        arcs[i]->destroy();
+      }
+      if (arcs != 0)
+        delete []arcs;
+      status = gr_not_init;
+    }
+
     void readGraph (const char *);
     void prettyPrint () const;
   private:
@@ -46,19 +63,12 @@ class Graph {
         throw("ERROR: Graph already initialized");
       nodes = new NodeType[nn];
       arcs = new ArcPointer*[nn];
+      num_nodes = nn;
+      num_arcs = 0;
       for (size_t i = 0; i < nn; i++) {
         ArcType info = ArcType();
         arcs[i] = new ArcPointer(info, 0, i, nn);
       }
-    }
-    void destroy () {
-      if (nodes != 0)
-        delete []nodes;
-      for (size_t i = 0; i < num_nodes; i++) {
-        arcs[i]->destroy();
-      }
-      if (arcs != 0)
-        delete []arcs;
     }
     void addArc (ArcType info, size_t from, size_t to) {
       ArcPointer *arc = new ArcPointer(info, 0, from, to);
@@ -72,6 +82,7 @@ class Graph {
       else
         q->next = arc;
       arc->next = p;
+      num_arcs++;
     }
 };
 
@@ -85,13 +96,15 @@ class Graph {
 template <typename NodeType, typename ArcType>
 void Graph<NodeType, ArcType>::readGraph (const char *filename) {
   std::ifstream file(filename);
-  file >> num_nodes >> num_arcs;
+  file >> num_nodes;
   init(num_nodes);
+  size_t na;
+  file >> na;
   for (size_t i = 0; i < num_nodes; i++)
     file >> nodes[i];
   size_t from, to;
   ArcType info;
-  for (size_t k = 0; k < num_arcs; k++) {
+  for (size_t k = 0; k < na; k++) {
     file >> from >> to >> info;
     this->addArc(info, from, to);
   }
